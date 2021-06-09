f50f77df5b4de8a9e35dd8e96651d26fb15bfb39
unknown <pawarmayuri681@gmail.com>
2021-06-09 18:44:44 +0530

    first commit

---
+++ Task123.py
@@ -0,0 +1,32 @@
+#Python program to Add Two Numbers Value Entered by User
+
+a = int(input("enter first number: "))
+b = int(input("enter second number: "))
+sum = a + b
+print("sum:", sum)
+
+
+
+
+# Python program to return maximum of two numbers
+num1 = int(input("Enter the num1: "))
+num2 = int(input("Enter the num2: "))
+maxVal = max(num1, num2)
+print("The maximum of all values is", maxVal)
+
+
+
+# Python program to return maximum of two numbers Using Math Function
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+if(num1 > num2):
+  print(num1, "is greater")
+elif(num1 < num2):
+    print(num2, "is greater")
+else:
+    print("Both are equal")
+
+
+
+
+
